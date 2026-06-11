7432c157704d5e7af34b2215483a2abd9c2156db
kenway214 <kenway214@outlook.com>
2026-06-11 15:13:09 +0530

    version: Bump ASCP version to v5.5
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- config/version.mk
+++ config/version.mk
@@ -3,7 +3,7 @@ CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 CUSTOM_PLATFORM_VERSION := 16.2
 CUSTOM_VERSION_PROP := sixteen
 
-ASCP_VERSION := 5.4
+ASCP_VERSION := 5.5
 ASCP_MAINTAINER ?= Unofficial Maintainer
 ASCP_OFFICIAL ?= true
 
