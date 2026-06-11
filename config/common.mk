LeafOS-Project/android_vendor_leaf

vendor: Introduce App Lock [4/4]
Change-Id: Ie6d86e301efe14146c961b11bfccd3fa74a1562c

config/common.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/leaf/overlay/common \
     vendor/leaf/overlay/no-rro
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/leaf/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # PDF
 PRODUCT_PACKAGES += \
     MuPDF

ascp-oss/vendor_custom

Signed-off-by: kenway214 <kenway214@outlook.com>

config/common.mk
@@ -81,6 +81,10 @@ PRODUCT_PACKAGES += \
     GoogleDialerOverlayCustom \
     SettingsOverlayCustom
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/custom/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # TouchGesture
 PRODUCT_PACKAGES += \
     TouchGestures